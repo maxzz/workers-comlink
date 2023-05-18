df3af77cb5b926b54e7b8e245ff57122d4bd8c6b
Max Zakharzhevskiy <maxzz@msn.com>
2023-05-18 15:35:00 -0700

    WIP. add comlink package

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { comlink } from 'vite-plugin-comlink';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,12 @@ export default defineConfig({
         port: 3000,
     },
     plugins: [
-        react()
+        react(),
+        comlink(),
     ],
+    worker: {
+        plugins: [
+            comlink(),
+        ]
+    }
 });
